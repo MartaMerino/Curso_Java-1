9713cb4043c766229e47c64c686dea294d6a96f6
Usuario-PC\Usuario <pablofernandezmena@gmail.com>
2018-02-13 12:48:19 +0100

    Subiendo cosas

---
+++ Practicas java/ProgramaCinev2.bat
@@ -0,0 +1,9 @@
+echo "Vamos a compilar"
+del "ProgramaCinev2.class"
+
+"C:\Program Files\Java\jdk1.8.0_161\bin\javac.exe" "ProgramaCinev2.java"
+
+"C:\Program Files\Java\jre1.8.0_91\bin\java.exe" ProgramaCinev2
+
+
+
